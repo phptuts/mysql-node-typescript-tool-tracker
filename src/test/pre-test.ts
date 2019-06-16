288eba4be47bfdb9e9935fbefbf96e81a99a13e8
Noah Glaser <glaserpower@gmail.com>
2019-06-15 21:10:18 -0700

    removing bad log

--- src/test/pre-test.ts
+++ src/test/pre-test.ts
@@ -4,8 +4,6 @@ import path from 'path';
 
 
 beforeAll(() => {
-	console.log('this should run');
-
 	const testConfigPath = path.join(__dirname , '..','..', `.env-${process.env['ENVIRONMENT_NAME']}`);
 	dotenv.config({ path:  testConfigPath });
 });
